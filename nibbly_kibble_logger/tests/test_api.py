2a8cfe5738b8974db3c2fb9b6e5d75e91e2518c9
Antar1011 <summer.kenster@gmail.com>
2020-06-08 21:59:11 -0400

    Test proper timestamping

--- nibbly_kibble_logger/tests/test_api.py
+++ nibbly_kibble_logger/tests/test_api.py
@@ -1,6 +1,9 @@
 """Test of the Nibbly Kibble Logger REST API"""
+import datetime as dt
 import json
 import pytest
+
+from unittest.mock import MagicMock
 
 from flask.cli import ScriptInfo
 from nibbly_kibble_logger.app import create_app
@@ -28,7 +31,6 @@ def client(app):
 class TestPost:
 
     def test_post_plaintext(self, client, record_filename):
-
         resp = client.post('/api/record', data='3.14', content_type='text/plain')
         assert resp.status == '200 OK'
 
@@ -37,7 +39,6 @@ class TestPost:
         assert json.loads(recorded_data[0])['drag time'] == 3.14
 
     def test_post_bad_plaintext(self, client):
-
         resp = client.post('/api/record', data="three years", content_type='text/plain')
         assert resp.status == '400 BAD REQUEST'
 
@@ -60,7 +61,6 @@ class TestPost:
                                                 'loser status': 'dnf'})
         assert resp.status == '400 BAD REQUEST'
 
-
     def test_post_appends_to_existing_file(self, client, record_filename):
         with open(record_filename, 'w') as f:
             f.write('messing up your logs\n')
@@ -72,3 +72,17 @@ class TestPost:
         assert len(recorded_data) == 2
         assert json.loads(recorded_data[1])['drag time'] == 3.14
         assert recorded_data[0] == 'messing up your logs\n'
+
+    def test_timestamping(self, client, record_filename, monkeypatch):
+        # solution adapted from https://stackoverflow.com/a/60629703
+        datetime_mock = MagicMock(wraps=dt.datetime)
+        datetime_mock.now.return_value = dt.datetime(2020, 6, 8, 22, 30, 18)
+        monkeypatch.setattr(dt, "datetime", datetime_mock)
+
+        resp = client.post('/api/record', data='3.14', content_type='text/plain')
+        assert resp.status == '200 OK'
+
+        recorded_data = open(record_filename).readlines()
+        assert len(recorded_data) == 1
+        assert json.loads(recorded_data[0]) == {'drag time': 3.14,
+                                                'timestamp': '2020-06-08 22:30:17'}
